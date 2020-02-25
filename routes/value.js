f244e630b8fea86656fed820af2f67c4a9ca1ab7
Maxim Piven <shpax1@gmail.com>
2020-02-25 16:56:28 +0200

    added post && get requests

--- routes/value.js
+++ routes/value.js
@@ -2,13 +2,28 @@ var express = require('express');
 var router = express.Router();
 const db = require('./db');
 
-/* GET users listing. */
 router.get('/:newValue', function(req, res, next) {
   const data = db.getData();
   data.counter = req.params.newValue;
   console.log('increment', data);
   db.setData(data);
   res.send('OK');
+});
+
+// copy of get request
+router.post('/:newValue', function(req, res, next) {
+  const data = db.getData();
+  data.counter = req.params.newValue;
+  console.log('increment', data);
+  db.setData(data);
+  res.send('OK');
+});
+
+
+
+router.get('/', function(req, res, next) {
+  const data = db.getData();
+  res.send(data);
 });
 
 module.exports = router;
